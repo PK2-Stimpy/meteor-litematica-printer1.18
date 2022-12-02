31a15cc0289b001e0a6a89915eac8d22483ad3cd
PK2_Stimpy <thefernan1@gmail.com>
2022-12-02 01:33:04 +0100

    Revert "Merge pull request #13 from Wide-Cat/main"
    This reverts commit 2a287f45e0ebb0159852b990d1a175955c5dcb14, reversing
    changes made to 34840b3d6dcc6786bb2450596f79d45093eff31d.

--- src/main/java/com/kkllffaa/meteor_litematica_printer/Printer.java
+++ src/main/java/com/kkllffaa/meteor_litematica_printer/Printer.java
@@ -24,9 +24,7 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.Pair;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.Vec3i;
-import net.minecraft.util.registry.Registry;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -34,12 +32,15 @@ import java.util.List;
 import java.util.function.Supplier;
 
 public class Printer extends Module {
+
+
 	private final SettingGroup sgGeneral = settings.getDefaultGroup();
-    private final SettingGroup sgRendering = settings.createGroup("Rendering");
+
+	//region settings
 
 	private final Setting<Integer> printing_range = sgGeneral.add(new IntSetting.Builder()
 			.name("printing-range")
-			.description("The block place range.")
+			.description("printing block place range.")
 			.defaultValue(2)
 			.min(1).sliderMin(1)
 			.max(6).sliderMax(6)
@@ -48,7 +49,7 @@ public class Printer extends Module {
 
 	private final Setting<Integer> printing_delay = sgGeneral.add(new IntSetting.Builder()
 			.name("printing-delay")
-			.description("Delay between printing blocks in ticks.")
+			.description("delay between printing blocks in ticks.")
 			.defaultValue(2)
 			.min(0).sliderMin(0)
 			.max(100).sliderMax(40)
@@ -57,154 +58,160 @@ public class Printer extends Module {
 
 	private final Setting<Integer> bpt = sgGeneral.add(new IntSetting.Builder()
 			.name("blocks/tick")
-			.description("How many blocks place per tick.")
+			.description("how many blocks place in 1 tick.")
 			.defaultValue(1)
 			.min(1).sliderMin(1)
 			.max(100).sliderMax(100)
 			.build()
 	);
 
-	private final Setting<Boolean> advanced = sgGeneral.add(new BoolSetting.Builder()
+	private final Setting<Boolean> advenced = sgGeneral.add(new BoolSetting.Builder()
 			.name("advanced")
-			.description("Respect block rotation (places blocks in weird places in singleplayer, multiplayer should work fine).")
+			.description("respect block rotation (places blocks in weird places only in singleplayer, multiplayer works fine).")
 			.defaultValue(false)
 			.build()
 	);
 
 	private final Setting<Boolean> swing = sgGeneral.add(new BoolSetting.Builder()
 			.name("swing")
-			.description("Swing hand when placing.")
+			.description("swing hand when placing.")
 			.defaultValue(false)
 			.build()
 	);
 
-    private final Setting<Boolean> returnHand = sgGeneral.add(new BoolSetting.Builder()
-			.name("return-slot")
-			.description("Return to old slot.")
+    private final Setting<Boolean> returnhand = sgGeneral.add(new BoolSetting.Builder()
+			.name("return slot")
+			.description("return to old slot.")
 			.defaultValue(false)
 			.build()
     );
 
-    private final Setting<Boolean> rotate = sgGeneral.add(new BoolSetting.Builder()
+	private final Setting<Boolean> renderblocks = sgGeneral.add(new BoolSetting.Builder()
+			.name("render placed blocks")
+			.description("render cube when placing block.")
+			.defaultValue(false)
+			.build()
+	);
+
+	private final Setting<Integer> fadetime = sgGeneral.add(new IntSetting.Builder()
+			.name("fade time")
+			.description("in ticks.")
+			.defaultValue(2)
+			.min(1).sliderMin(1)
+			.max(1000).sliderMax(100)
+			.visible(renderblocks::get)
+			.build()
+	);
+
+	private final Setting<SettingColor> color = sgGeneral.add(new ColorSetting.Builder()
+			.name("color")
+			.description("cubes color.")
+			.defaultValue(new SettingColor(100, 100, 100))
+			.visible(renderblocks::get)
+			.build()
+	);
+
+    private final Setting<Boolean> rotateblocks = sgGeneral.add(new BoolSetting.Builder()
 			.name("rotate")
-			.description("Rotate to the blocks being placed.")
+			.description("look at the blocks being placed.")
 			.defaultValue(false)
 			.build()
     );
-
-    private final Setting<SortAlgorithm> firstAlgorithm = sgGeneral.add(new EnumSetting.Builder<SortAlgorithm>()
-			.name("first-sorting-mode")
+    
+    private final Setting<SortAlgoritm> algoritm = sgGeneral.add(new EnumSetting.Builder<SortAlgoritm>()
+			.name("sorting mode")
 			.description("The blocks you want to place first.")
-			.defaultValue(SortAlgorithm.None)
+			.defaultValue(SortAlgoritm.None)
 			.build()
 	);
-
-    private final Setting<SortingSecond> secondAlgorithm = sgGeneral.add(new EnumSetting.Builder<SortingSecond>()
-			.name("second-sorting-mode")
-			.description("Second pass of sorting eg. place first blocks higher and closest to you.")
+    
+    private final Setting<SortingSecond> secondalgoritn = sgGeneral.add(new EnumSetting.Builder<SortingSecond>()
+			.name("second sorting mode")
+			.description("second pass of sorting eg. place first blocks higher and closest to you.")
 			.defaultValue(SortingSecond.None)
-			.visible(()-> firstAlgorithm.get().applySecondSorting)
+			.visible(()->algoritm.get().applysecondsorting)
 			.build()
 	);
+	
 
-    private final Setting<Boolean> renderBlocks = sgRendering.add(new BoolSetting.Builder()
-        .name("render-placed-blocks")
-        .description("Renders block placements.")
-        .defaultValue(true)
-        .build()
-    );
-
-    private final Setting<Integer> fadeTime = sgRendering.add(new IntSetting.Builder()
-        .name("fade-time")
-        .description("Time for the rendering to fade, in ticks.")
-        .defaultValue(3)
-        .min(1).sliderMin(1)
-        .max(1000).sliderMax(20)
-        .visible(renderBlocks::get)
-        .build()
-    );
-
-    private final Setting<SettingColor> colour = sgRendering.add(new ColorSetting.Builder()
-        .name("colour")
-        .description("The cubes colour.")
-        .defaultValue(new SettingColor(95, 190, 255))
-        .visible(renderBlocks::get)
-        .build()
-    );
-
-    private int timer;
-    private int usedSlot = -1;
-    private final List<BlockPos> toSort = new ArrayList<>();
-    private final List<Pair<Integer, BlockPos>> placed_fade = new ArrayList<>();
-
+	//endregion
+	
 
 	public Printer() {
-		super(Addon.CATEGORY, "litematica-printer", "Automatically prints open schematics");
+		super(Addon.CATEGORY, "litematica-printer", "description");
 	}
 
-    @Override
-    public void onActivate() {
-        onDeactivate();
-    }
+	private int timer;
+	private int usedslot = -1;
+	
+	private final List<BlockPos> tosort = new ArrayList<>();
 
-	@Override
-    public void onDeactivate() {
+	private final List<Pair<Integer, BlockPos>> placed_fade = new ArrayList<>();
+
+	@Override public void onDeactivate() {
 		placed_fade.clear();
 	}
-
-	@EventHandler
+	@Override public void onActivate() {
+		onDeactivate();
+	}
+	
+	@EventHandler @SuppressWarnings("unused")
 	private void onTick(TickEvent.Post event) {
 		if (mc.player == null || mc.world == null) {
 			placed_fade.clear();
 			return;
 		}
-
-		placed_fade.forEach(s -> s.setLeft(s.getLeft() - 1));
+		
+		placed_fade.forEach(s -> s.setLeft(s.getLeft()-1));
 		placed_fade.removeIf(s -> s.getLeft() <= 0);
-
+		
 		WorldSchematic worldSchematic = SchematicWorldHandler.getSchematicWorld();
 		if (worldSchematic == null) {
 			placed_fade.clear();
 			toggle();
 			return;
 		}
-
-		toSort.clear();
-
+		
+		tosort.clear();
+		
 		if (timer >= printing_delay.get()) {
-			BlockIterator.register(printing_range.get() + 1, printing_range.get() + 1, (pos, blockState) -> {
+			
+			BlockIterator.register(printing_range.get()+1, printing_range.get()+1, (pos, blockState) -> {
 				BlockState required = worldSchematic.getBlockState(pos);
 
+				
 				if (mc.player.getBlockPos().isWithinDistance(pos, printing_range.get()) &&
-                        blockState.getMaterial().isReplaceable() && !required.isAir() && blockState.getBlock() != required.getBlock() &&
-                        DataManager.getRenderLayerRange().isPositionWithinRange(pos) &&
-                        !mc.player.getBoundingBox().intersects(Vec3d.of(pos), Vec3d.of(pos).add(1, 1, 1))) {
-					toSort.add(new BlockPos(pos));
+						blockState.isAir() && !required.isAir() && blockState.getBlock() != required.getBlock() &&
+						DataManager.getRenderLayerRange().isPositionWithinRange(pos)) {
+					tosort.add(new BlockPos(pos));
 				}
 			});
-
+			
+			
 			BlockIterator.after(() -> {
 				//if (!tosort.isEmpty()) info(tosort.toString());
-
-				if (firstAlgorithm.get() != SortAlgorithm.None) {
-					if (firstAlgorithm.get().applySecondSorting) {
-						if (secondAlgorithm.get() != SortingSecond.None) {
-							toSort.sort(secondAlgorithm.get().algorithm);
+				
+				
+				
+				if (algoritm.get() != SortAlgoritm.None) {
+					if (algoritm.get().applysecondsorting) {
+						if (secondalgoritn.get() != SortingSecond.None) {
+							tosort.sort(secondalgoritn.get().al);
 						}
 					}
-					toSort.sort(firstAlgorithm.get().algorithm);
+					tosort.sort(algoritm.get().al);
 				}
-
-
+				
+				
 				int placed = 0;
-				for (BlockPos pos : toSort) {
-
+				for (var pos : tosort) {
+					
 					BlockState state = worldSchematic.getBlockState(pos);
-					if (switchItem(state.getBlock().asItem(), () -> place(state, pos))) {
+					if (swichitem(state.getBlock().asItem(), () -> place(state, pos, advenced.get(), swing.get()))) {
 						timer = 0;
 						placed++;
-						if (renderBlocks.get()) {
-							placed_fade.add(new Pair<>(fadeTime.get(), new BlockPos(pos)));
+						if (renderblocks.get()) {
+							placed_fade.add(new Pair<>(fadetime.get(), new BlockPos(pos)));
 						}
 						if (placed >= bpt.get()) {
 							return;
@@ -212,91 +219,81 @@ public class Printer extends Module {
 					}
 				}
 			});
-
-
-		} else timer++;
+			
+			
+		}else timer++;
 	}
 
-	public boolean place(BlockState required, BlockPos pos) {
+	public boolean place(BlockState required, BlockPos pos, boolean adv, boolean _swing) {
 		if (mc.player == null || mc.world == null) return false;
-		if (!mc.world.getBlockState(pos).getMaterial().isReplaceable()) return false;
 
-        Direction direction = dir(required);
+		if (mc.world.isAir(pos) || mc.world.getBlockState(pos).getMaterial().isLiquid()) {
+			Direction direction = dir(required);
 
-        if (!advanced.get() || direction == Direction.UP) {
-            return BlockUtils.place(pos, Hand.MAIN_HAND, mc.player.getInventory().selectedSlot, rotate.get(), 50, swing.get(), true, false);
-        } else {
-            return MyUtils.place(pos, direction, swing.get(), rotate.get());
-        }
-	}
-
-	private boolean switchItem(Item item, Supplier<Boolean> action) {
-		if (mc.player == null) return false;
-
-		int a = mc.player.getInventory().selectedSlot;
-		FindItemResult result = InvUtils.find(item);
-
-		if (mc.player.getMainHandStack().getItem() == item) {
-			if (action.get()) {
-				usedSlot = mc.player.getInventory().selectedSlot;
-				return true;
-			} else return false;
-
-		} else if (usedSlot != -1 && mc.player.getInventory().getStack(usedSlot).getItem() == item) {
-			InvUtils.swap(usedSlot, returnHand.get());
-
-			if (action.get()) {
-				InvUtils.swap(a, returnHand.get());
-				return true;
-			} else {
-				InvUtils.swap(a, returnHand.get());
-				return false;
+			if (!adv || direction == Direction.UP) {
+				return BlockUtils.place(pos, Hand.MAIN_HAND, mc.player.getInventory().selectedSlot, false, 50, _swing, true, false);
+			}else {
+				return MyUtils.place(pos, direction, _swing, rotateblocks.get());
 			}
 
-		} else if (result.found()) {
-			if (result.isHotbar()) {
-				InvUtils.swap(result.slot(), returnHand.get());
+		}else return false;
+	}
 
+	private boolean swichitem(Item item, Supplier<Boolean> action) {
+		if (mc.player == null) return false;
+		int a = mc.player.getInventory().selectedSlot;
+		FindItemResult result = InvUtils.find(item);
+		if (mc.player.getMainHandStack().getItem() == item) {
+			if (action.get()) {
+				usedslot = mc.player.getInventory().selectedSlot;
+				return true;
+			}else return false;
+		} else if (usedslot != -1 && mc.player.getInventory().getStack(usedslot).getItem() == item) {
+			InvUtils.swap(usedslot, returnhand.get());
+			if (action.get()) {
+				InvUtils.swap(a, returnhand.get());
+				return true;
+			}else {
+				InvUtils.swap(a, returnhand.get());
+				return false;
+			}
+		}else if (result.found()) {
+			if (result.isHotbar()) {
+				InvUtils.swap(result.slot(), returnhand.get());
 				if (action.get()) {
-					usedSlot = mc.player.getInventory().selectedSlot;
-					InvUtils.swap(a, returnHand.get());
+					usedslot = mc.player.getInventory().selectedSlot;
+					InvUtils.swap(a, returnhand.get());
 					return true;
-				} else {
-					InvUtils.swap(a, returnHand.get());
+				}else {
+					InvUtils.swap(a, returnhand.get());
 					return false;
 				}
-
-			} else if (result.isMain()) {
+			}else if (result.isMain()){
 				FindItemResult empty = InvUtils.findEmpty();
-
 				if (empty.found() && empty.isHotbar()) {
 					InvUtils.move().from(result.slot()).toHotbar(empty.slot());
-					InvUtils.swap(empty.slot(), returnHand.get());
-
+					InvUtils.swap(empty.slot(), returnhand.get());
 					if (action.get()) {
-						usedSlot = mc.player.getInventory().selectedSlot;
-						InvUtils.swap(a, returnHand.get());
+						usedslot = mc.player.getInventory().selectedSlot;
+						InvUtils.swap(a, returnhand.get());
 						return true;
-					} else {
-						InvUtils.swap(a, returnHand.get());
+					}else {
+						InvUtils.swap(a, returnhand.get());
 						return false;
 					}
-
-				} else if (usedSlot != -1) {
-					InvUtils.move().from(result.slot()).toHotbar(usedSlot);
-					InvUtils.swap(usedSlot, returnHand.get());
-
+				} else if (usedslot != -1) {
+					InvUtils.move().from(result.slot()).toHotbar(usedslot);
+					InvUtils.swap(usedslot, returnhand.get());
 					if (action.get()) {
-						InvUtils.swap(a, returnHand.get());
+						InvUtils.swap(a, returnhand.get());
 						return true;
-					} else {
-						InvUtils.swap(a, returnHand.get());
+					}else {
+						InvUtils.swap(a, returnhand.get());
 						return false;
 					}
-
-				} else return false;
-			} else return false;
-		} else return false;
+				}else return false;
+			}else return false;
+		}else return false;
 	}
 
 	private Direction dir(BlockState state) {
@@ -306,42 +303,43 @@ public class Printer extends Module {
 		else return Direction.UP;
 	}
 
-	@EventHandler
+	@EventHandler @SuppressWarnings("unused")
 	private void onRender(Render3DEvent event) {
 		placed_fade.forEach(s -> {
-			Color a = new Color(colour.get().r, colour.get().g, colour.get().b, (int) (((float)s.getLeft() / (float) fadeTime.get()) * colour.get().a));
+			Color a = new Color(color.get().r, color.get().g, color.get().b, (int) (((float)s.getLeft() / (float)fadetime.get())*color.get().a));
 			event.renderer.box(s.getRight(), a, null, ShapeMode.Sides, 0);
 		});
 	}
-
+	
 	@SuppressWarnings("unused")
-	public enum SortAlgorithm {
+	public enum SortAlgoritm {
 		None(false, (a, b) -> 0),
-		TopDown(true, Comparator.comparingInt(value -> value.getY() * -1)),
+		TopDown(true, Comparator.comparingInt(value -> value.getY()*-1)),
 		DownTop(true, Comparator.comparingInt(Vec3i::getY)),
 		Closest(false, Comparator.comparingDouble(value -> MeteorClient.mc.player != null ? Utils.squaredDistance(MeteorClient.mc.player.getX(), MeteorClient.mc.player.getY(), MeteorClient.mc.player.getZ(), value.getX() + 0.5, value.getY() + 0.5, value.getZ() + 0.5) : 0)),
-		Furthest(false, Comparator.comparingDouble(value -> MeteorClient.mc.player != null ? (Utils.squaredDistance(MeteorClient.mc.player.getX(), MeteorClient.mc.player.getY(), MeteorClient.mc.player.getZ(), value.getX() + 0.5, value.getY() + 0.5, value.getZ() + 0.5)) * -1 : 0));
-
-
-		final boolean applySecondSorting;
-		final Comparator<BlockPos> algorithm;
-
-		SortAlgorithm(boolean applySecondSorting, Comparator<BlockPos> algorithm) {
-			this.applySecondSorting = applySecondSorting;
-			this.algorithm = algorithm;
+		Furthest(false, Comparator.comparingDouble(value -> MeteorClient.mc.player != null ? (Utils.squaredDistance(MeteorClient.mc.player.getX(), MeteorClient.mc.player.getY(), MeteorClient.mc.player.getZ(), value.getX() + 0.5, value.getY() + 0.5, value.getZ() + 0.5))*-1 : 0));
+		
+		
+		final boolean applysecondsorting;
+		final Comparator<BlockPos> al;
+		
+		SortAlgoritm(boolean applysecondsorting, Comparator<BlockPos> al) {
+			this.applysecondsorting = applysecondsorting;
+			this.al = al;
 		}
 	}
-
+	
 	@SuppressWarnings("unused")
 	public enum SortingSecond {
-		None(SortAlgorithm.None.algorithm),
-		Nearest(SortAlgorithm.Closest.algorithm),
-		Furthest(SortAlgorithm.Furthest.algorithm);
-
-		final Comparator<BlockPos> algorithm;
-
-		SortingSecond(Comparator<BlockPos> algorithm) {
-			this.algorithm = algorithm;
+		None(SortAlgoritm.None.al),
+		Nearest(SortAlgoritm.Closest.al),
+		Furthest(SortAlgoritm.Furthest.al);
+		
+		final Comparator<BlockPos> al;
+		
+		SortingSecond(Comparator<BlockPos> al) {
+			this.al = al;
 		}
 	}
+	
 }
